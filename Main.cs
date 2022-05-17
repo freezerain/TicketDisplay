2232bad70da871910169fa4da8768b8becfb990b
Ilia The Rat King <rain-ilia@Rambler.ru>
2022-05-17 19:11:53 +0200

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Diagnostics.Debug;
+namespace TicketDisplay
+{
+    public class Main
+    {
+        public Main()
+        {
+            WriteLine("This object was created");
+        }
+        
+        public object[] ListaProcedimientos()
+        {
+            WriteLine("Event list registered");
+            return new object[] { "Iniciar", "Finalizar", "AntesDeGuardarMaestro" , "AntesDeGuardarMaestroV2"
+            ,"DespuesDeCargarDocumento", "ActivarMetodosAlServir", "SePuedeRealizarAccion"
+            ,"AntesDeCambioDeCampoEnDocumentoProc", "AlValidarClienteProveedor", "DespuesDeGuardarLinea"
+            ,/*"AntesDeGuardarDocumentoV2",*/ "DespuesDeGuardarDocumento", "DespuesDeCancelarDocumento",
+            "DespuesDeGuardarEfecto", "DespuesDeGuardarRemesa", "DespuesDeGuardarAsiento"
+            , "CamposAEscucharEnDocumento", "DespuesDeCambioDeCampoEnDocumento"
+            };
+        }
+
+        object CamposAEscucharEnDocumento(string claseDocumento, float idDoc)
+        {
+            WriteLine("CamposAEscucharEnDocumento: " + claseDocumento + " "
+                          + idDoc );
+            return new object[] {2, "CABECERA.REFERENCIA", "LINEA.CODART"};
+        }
+
+        void DespuesDeCambioDeCampoEnDocumento(string claseDocumento, float idDoc, string
+            campo, object valorAnterior, object nuevoValor)
+        {
+            WriteLine("DespuesDeCambioDeCampoEnDocumento: " + claseDocumento + " "
+                      + idDoc + " " + campo+ " " + valorAnterior+ " " + nuevoValor);
+        }
+
+
+        void DespuesDeGuardarAsiento(float idAsiento, object asiento)
+        {
+            WriteLine("DespuesDeGuardarAsiento: " + idAsiento + " "
+              + asiento );
+        }
+
+        void DespuesDeGuardarRemesa(string operacion, string tipo, float idRemesa)
+        {
+            WriteLine("DespuesDeGuardarRemesa: " + operacion + " "
+              + tipo + " " + idRemesa);
+        }
+
+        void DespuesDeGuardarEfecto(string operacion, float numCartera, int numVen)
+        {
+            WriteLine("DespuesDeGuardarEfecto: " + operacion + " "
+              + numCartera + " " + numVen );
+        }
+
+        void DespuesDeCancelarDocumento(string documento, double idDoc, object cabecera,
+object lineas, int estado)
+        {
+            WriteLine("DespuesDeCancelarDocumento: " + documento + " "
+              + idDoc + " "+ cabecera + " " + lineas + " " + estado );
+        }
+
+        void DespuesDeGuardarDocumento(string documento, double idDoc)
+        {
+            WriteLine("DespuesDeGuardarDocumento: " + documento + " "
+              + idDoc);
+        }
+
+        /*
+        bool AntesDeGuardarDocumentoV2(string documento, double idDoc, object cabecera,
+object lineas, int estado)
+        {
+            System.Diagnostics.Debug.WriteLine("AntesDeGuardarDocumentoV2: " + documento + " "
+              + idDoc + " " + cabecera);
+            return true;
+        }
+        */
+
+        void DespuesDeGuardarLinea(string tipoDocumento, object cabecera, object linea)
+        {
+            WriteLine("DespuesDeGuardarLinea: " + tipoDocumento + " "
+               + cabecera + " " + linea );
+        }
+
+        void AntesDeCambioDeCampoEnDocumentoProc(string claseDocumento, float idDoc,
+string campo, object valorAnterior, ref object nuevoValor)
+
+        {
+            WriteLine("AntesDeCambioDeCampoEnDocumentoProc: " + claseDocumento + " "
+                + idDoc + " " + campo + " " + valorAnterior + " " + nuevoValor);
+        }
+
+        void SePuedeRealizarAccion(string TipoDocumento, double idDoc, int Accion, ref int
+aRespuesta, ref string aMotivo)
+        {
+            WriteLine("SePuedeRealizarAccion: " + TipoDocumento + " "
+                + idDoc + " " + Accion + " " + aRespuesta + " " + aMotivo);
+        }
+
+        bool ActivarMetodosAlServir()
+        {
+            WriteLine("ActivarMetodosAlServir ");
+            return true;
+        }
+
+        void DespuesDeCargarDocumento(string tipoDoc, float IdDoc, object cabecera, object
+lineas, int estado)
+        {
+            WriteLine("DespuesDeCargarDocumento: " + tipoDoc + " " 
+                + IdDoc + " " + cabecera + " " + lineas + " " + estado);
+        }
+
+        bool AntesDeGuardarMaestroV2(string tabla, ref object datos, int estado)
+        {
+            WriteLine("AntesDeGuardarMaestroV2: " + tabla + " " + datos+ " "+ estado);
+            return false;
+        }
+
+        bool AntesDeGuardarMaestro(string tabla, ref object datos)
+        {
+            WriteLine("AntesDeGuardarMaestro: " + tabla + " " + datos);
+            return false;
+        }
+
+        public void Iniciar(string conexionSistema, string conexionEmpresa)
+        {
+            WriteLine("Init event with " + conexionEmpresa);
+        }
+        
+        public void Finalizar()
+        {
+            WriteLine("Finilazed event");
+        }
+        
+    }
+}
---
+++ README.txt
@@ -0,0 +1,15 @@
+﻿This project will show scanned articles 
+for each ticket created in TPV window of a3erp
+Author: Ilia Rodikov
+Company: SGE
+Date: 27.04.2022
+
+How to build: 
+1a) Change build directory of the project to valid path(same as in "How to use" section)
+1b) Alternativly build in any folder and then move DLL to valid path root
+2) Make sure target CPU is x86
+
+How to use:
+1) Put dll to valid path "<directorioERP>\Extensiones\SGE\TicketDisplay\Binarios"
+2) Register DLL in database using SQL View (code in "SQL_View_DLL_Register" file of this project) for each company that  wants to use it.
+3) Put Regasm assembly file  (RegDlls.ini of this project) in same folder 
